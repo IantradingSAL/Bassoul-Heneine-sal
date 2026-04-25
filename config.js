e462d5080f8dc522893916dc9346919dbfc7ca09
IantradingSAL <imadaehn@gmail.com>
2026-04-25 14:43:02 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 export const SUPABASE_CONFIG = {
-  url:     'https://<project-ref>.supabase.co',
-  anonKey: 'eyJhbGc...<your anon key>'
+  url:     'https://egcbbfiqzaccsquaytvz.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVnY2JiZmlxemFjY3NxdWF5dHZ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcxMDE1MzgsImV4cCI6MjA5MjY3NzUzOH0.2qLR3uLTE8C2tKSQUEV-9q2DF8Hrfad08UQoLv5Q72c'
 }
